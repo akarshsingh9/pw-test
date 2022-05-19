f96a9f88ae467702c6ce78a5b840f6dfc93e4708
Akarsh Singh <akarsh.singh9@gmail.com>
2022-05-19 11:59:36 +0530

    images replaced

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Projects that I have worked on from the year 2022 onwards. 
           </p>
         </div>
         <div className="container py-12">
